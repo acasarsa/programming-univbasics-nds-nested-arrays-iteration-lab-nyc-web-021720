d1301ca30ca8082be8f8261ad2b87a48cb5a478e
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-03 22:49:14 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -31,9 +31,14 @@ def total_even_pairs(src)
   total = 0
 
   while row_index < src.count do
+    if src[row_index][0] %2 == 0 && src[row_index][1] %2 == 0
+      total += src[row_index][0] + src[row_index][1]
 
+    end
+    total += 1
+    row_index += 1
   end
-
+  total
 end
 
 # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
